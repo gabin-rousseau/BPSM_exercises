24f6f1c6036339999ab83d528c01b951d802c3c8
gabin-rousseau <rousseau.gabin.off@gmail.com>
2024-10-11 12:54:40 +0100

    Added Lecture08 content

---
+++ Lecture08/myanalysisprogram.sh
@@ -0,0 +1,8 @@
+myfavouritesequence='ACATAAAACATCAAAGTGAACAGATTGTAGTGTAAGAAGTTAGATTAA' 
+while read line
+do read line2
+ if [[ ${line2} == *${myfavouritesequence}* ]] ;
+ then
+  echo "The sequence was found in" ${line} ${line2}
+ fi  
+done < $1
