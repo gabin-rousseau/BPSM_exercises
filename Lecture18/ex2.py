8d2fe756e408a19205b05c5b503923a2d46389b7
gabin-rousseau <rousseau.gabin.off@gmail.com>
2024-11-19 10:31:08 +0000

    Updated ex2.py from L18

--- Lecture18/ex2.py
+++ Lecture18/ex2.py
@@ -3,7 +3,7 @@
 import re
 
 dna_file=open('long_dna.txt')
-dna=dna_file.read()
+dna=dna_file.read().rstrip("\n")
 dna_file.close
 
 #in this exercise, using regex splitting would remove the recognition pattern from fragments, which is not what
@@ -17,11 +17,18 @@ for run in dig1:
 	slice_ids.append(run.start() + 3)
 
 fragments=[dna]
+
+print(f"\nCut at sites {slice_ids}.\n")
+
+cut_length=0
+
 for i in slice_ids:
+	i-=cut_length
 	cut_fragment=fragments[-1]
 	fragments.pop()
 	fragments.append(cut_fragment[:i])
 	fragments.append(cut_fragment[i:])
+	cut_length+=len(cut_fragment[:i])
 print('Fragments from BpsmI digestion.')
 print(fragments)
 
